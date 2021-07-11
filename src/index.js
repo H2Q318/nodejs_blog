609f7701cc0abc7eb09520ec8a572b6e23ae8c08
Hunine <hqhung201@gmail.com>
2021-07-11 09:18:50 +0700

    Add file routes and file controllers

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
 const path = require('path');
 const express = require('express')
 const morgan = require('morgan')
-const handlebars  = require('express-handlebars');
-const app = express()
-const port = 3000
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+//XMLHttpRequest, fetch, axios,...
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({extname: '.hbs'}));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-})
+});
